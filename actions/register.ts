7a580f74d2616826ab99b0e678acd6c7c5594ddf
kapepa <kapepayx@gmail.com>
2024-05-20 21:29:19 +0300

    database

--- actions/register.ts
+++ actions/register.ts
@@ -1,9 +1,29 @@
+"use server"
+
 import { RegisterSchema } from "@/schemas"
 import { z } from "zod"
+import { hash } from "bcryptjs"
+import prisma from "@/lib/db"
+import { getUserByEmail } from "@/services/user"
 
 const register = async (value: z.infer<typeof RegisterSchema>) => {
   const result = RegisterSchema.safeParse(value);
   if (!result.success) return { error: "Invalid fields!" }
+
+  const { name, email, password } = result.data;
+  const hashedPassword = await hash(password, 10);
+
+  const existingUser = await getUserByEmail(email)
+
+  if (!!existingUser) return { error: "Email already in use!" }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email, 
+      password: hashedPassword, 
+    }
+  })
 
   return { success: "Created an account" }
 }
---
+++ services/user.ts
@@ -0,0 +1,32 @@
+import prisma from "@/lib/db";
+import { User } from "@prisma/client";
+
+const getUserByEmail = async (email: string): Promise<User | null> => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
+
+    return user
+  } catch {
+    return null;
+  }
+}
+
+const getUserById = async (id: string): Promise<User | null> => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id
+      }
+    });
+
+    return user
+  } catch {
+    return null;
+  }
+}
+
+export { getUserByEmail }
