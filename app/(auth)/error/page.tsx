5ebc35c5e8b90d78f21805c0d08144ce2ec80cdb
kapepa <kapepayx@gmail.com>
2024-05-23 10:06:27 +0300

    google github

---
+++ app/(auth)/error/page.tsx
@@ -0,0 +1,10 @@
+import { ErrorCard } from "@/components/auth/error-card";
+import { NextPage } from "next";
+
+const AuthErrorPage: NextPage = () => {
+  return (
+    <ErrorCard/>
+  )
+}
+
+export default AuthErrorPage;
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,11 +1,28 @@
 "use client"
 
-import { FC } from "react";
+import { FC, useTransition } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "../ui/button";
 
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { signIn } from "next-auth/react";
+
+enum SocialProvider {
+  Google = "google",
+  Github = "github",
+}
+
+type SocialProvidetype = SocialProvider;
+
 const Social: FC = () => {
+  const [isPending, startTransition] = useTransition()
+  const onClick = (provider: SocialProvidetype) => {
+    startTransition(async () => {
+      await signIn(provider, { redirectTo: DEFAULT_LOGIN_REDIRECT });
+    })
+  }
+
   return (
     <div
       className="flex items-center w-full gap-x-2"
@@ -14,7 +31,8 @@ const Social: FC = () => {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {}}
+        disabled={isPending}
+        onClick={onClick.bind(null, SocialProvider.Google)}
       >
         <FcGoogle
           className="h-5 w-5"
@@ -24,7 +42,8 @@ const Social: FC = () => {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {}}
+        disabled={isPending}
+        onClick={onClick.bind(null, SocialProvider.Github)}
       >
         <FaGithub
            className="h-5 w-5"
