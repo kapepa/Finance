48ad41e3be2a2abc15080ebc94b33358a2ad28a6
kapepa <kapepayx@gmail.com>
2024-06-04 21:13:24 +0300

    up

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -5,7 +5,7 @@ import { NextPage } from "next";
 
 const ServerPage: NextPage = async () => {
   // const user = await auth();
-  const user = await currentUser()
+  const user = await currentUser();
 
   return (
     <UserInfo
